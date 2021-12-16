b65acefe8e07e37bbea75ef34cf45a03118d2cfb
gabrielapl <gabrielp.a123456789@gmail.com>
2021-12-15 23:47:50 -0300

    Sistema de Auth

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,18 @@
+import { GetStaticProps } from 'next'
 import Head from 'next/head';
 import { SubscribeButton } from '../components/SubscribeButton';
+import { stripe } from '../services/stripe';
 
 import styles from './home.module.scss';
 
-export default function Home() {
+interface Product {
+  product: {
+    id: string;
+    amount: number;
+  }
+}
+
+export default function Home({ product }: Product) {
   return (
     <>
     <Head><title> Início | ig.news</title></Head>
@@ -13,12 +22,32 @@ export default function Home() {
         <h1>News about the <span>React</span> world.</h1>
         <p>
           Get access to al the publications <br />
-          <span> for $9.90 month</span>
+          <span> for {product.amount} month</span>
         </p>
-        <SubscribeButton />
+        <SubscribeButton priceId={product.id} />
       </section>
       <img src="/images/avatar.svg" alt="girl coding" />
     </main>
     </>
   )
 }
+
+export const getStaticProps: GetStaticProps = async () => {
+
+  const price = await stripe.prices.retrieve("price_1K6k9dCaa1MU6wKZdNkZ4ALE");
+
+  const product = {
+    princeId: price.id,
+    amount: new Intl.NumberFormat("en-US", {
+      style: 'currency',
+      currency: 'USD',
+    }).format( price.unit_amount / 100)
+  }
+
+  return {
+    props: {
+      product
+    },
+    revalidate: 60 * 60 * 24 // 24 Hours
+  }
+};
